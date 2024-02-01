1b0cf511aa092cacad47d9d9b9504a93c919272d
William Romine <lromine8@gmail.com>
2024-01-31 20:45:09 -0600

    Update

---
+++ Assignment02/include/newgraph.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <utility>
+#include <functional>
+template <class N>
+class Graph {
+public:
+    // Default constuctor, create empty
+    Graph() {};
+    Graph(std::vector<N> nodes, std::vector<std::pair<N, N>> edges) {};
+    virtual ~Graph() {};
+    
+    virtual bool adjacent(N x, N y) = 0;
+    virtual std::vector<N> neighbors(N x) = 0;
+    virtual void addNode(N x) = 0;
+    virtual void addEdge(N x, N y) = 0;
+    virtual void deleteEdge(N x, N y) = 0;
+    // Traversals
+    virtual void dfs(N startNode, std::function<void(N)> visit) = 0;
+    virtual void bfs(N startNode, std::function<void(N)> visit) = 0;
+};
